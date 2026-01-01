4123761dfd55dda8c6a8d072fb6757ffb7183ec8
SrBolasGrandes <canalpalomacabesuda@gmail.com>
2026-01-01 13:37:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from PIL import Image
-import base64, io, time, requests
+import base64, io, time, os, requests
 
 app = Flask(__name__)
 CORS(app)
@@ -12,42 +12,45 @@ TIMEOUT = 3
 last_frame = None
 last_time = 0
 
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 FALLBACK = "https://raw.githubusercontent.com/SrBolasGrandes/Camera-Voxel-Roblox/refs/heads/main/262%20Sem%20T%C3%ADtulo_20260101105003.png"
 
 def load_fallback():
     global last_frame
     r = requests.get(FALLBACK)
-    img = Image.open(io.BytesIO(r.content)).convert("RGB")
-    img = img.resize((GRID,GRID))
+    img = Image.open(io.BytesIO(r.content)).convert("RGB").resize((GRID,GRID))
     last_frame = list(img.getdata())
 
 @app.route("/")
-def home():
+def cam():
     return render_template("camera.html")
+
+@app.route("/video")
+def video():
+    return render_template("video.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    f = request.files["video"]
+    f.save(os.path.join(UPLOAD_DIR,"video.mp4"))
+    return "ok"
 
 @app.route("/camera", methods=["POST"])
 def camera():
-    global last_frame, last_time
+    global last_frame,last_time
 
     raw = base64.b64decode(request.json["image"])
-    mode = request.json.get("mode","color")
-
-    img = Image.open(io.BytesIO(raw)).convert("RGB")
-    img = img.resize((GRID,GRID))
-    pixels = list(img.getdata())
-
-    if mode == "bw":
-        pixels = [(l:=int(r*0.299+g*0.587+b*0.114),l,l) for r,g,b in pixels]
-
-    last_frame = pixels
+    img = Image.open(io.BytesIO(raw)).convert("RGB").resize((GRID,GRID))
+    last_frame = list(img.getdata())
     last_time = time.time()
 
     return jsonify(ok=True)
 
 @app.route("/cameraGet")
 def get():
-    global last_frame
-    if last_frame is None or time.time() - last_time > TIMEOUT:
+    if last_frame is None or time.time()-last_time>TIMEOUT:
         load_fallback()
 
     return jsonify(
